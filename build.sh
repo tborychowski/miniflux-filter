b643c22acf6f22b159bd0c811d2a1886e2b6a9b4
Dziad Borowy <github@borychowski.org>
2022-01-20 10:50:55 +0000

    bump version

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-# docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 -t tborychowski/miniflux-filter:latest -t tborychowski/miniflux-filter:2.0.1 .
 docker build --tag tborychowski/miniflux-filter:latest .
+# docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 -t tborychowski/miniflux-filter:latest -t tborychowski/miniflux-filter:2.0.2 .
